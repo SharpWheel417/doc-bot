86fe87c3efbfded5bf7b5372b6df26a9c0ea4f7d
Petrov Gleb <kotejackwhite@gmail.com>
2024-07-31 17:29:58 +0700

    update

--- src/view/send.py
+++ src/view/send.py
@@ -4,7 +4,7 @@ from telegram.ext import ContextTypes
 import telegram
 
 async def sendmess(text: any, update: Update, context: ContextTypes):
-  await context.bot.send_message(chat_id=update.effective_chat.id, text=str(text), parse_mode=telegram.ParseMode.MARKDOWN)
+  await context.bot.send_message(chat_id=update.effective_chat.id, text=str(text), parse_mode='Markdown')
 
 async def senddoc(f: any, text: any, update: Update, context: ContextTypes):
   await context.bot.send_document(chat_id=update.effective_chat.id, document=f, caption=str(text))
