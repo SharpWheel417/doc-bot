4af679997cd8335e239e7167789d9c78e31ac515
Petrov Gleb <kotejackwhite@gmail.com>
2024-07-31 16:13:35 +0700

    add terminal

--- src/controller/message.py
+++ src/controller/message.py
@@ -7,21 +7,19 @@ import subprocess
 from config import ADMIN_ID
 from ..view.send import sendmess, senddoc
 
-# Получаем путь к дефолтному терминалу
 terminal = subprocess.Popen(['gnome-terminal'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 async def handle_message(update: Update, context: CallbackContext):
+    global terminal
+    if context._user_id in ADMIN_ID:
+        text = update.message.text
+        # Выполняем команду
+        # Создаем новый экземпляр терминала
 
-  if context._user_id in ADMIN_ID:
+        # Отправляем команду в терминал
+        terminal.stdin.write(text.encode() + b'\n')
 
-    text = update.message.text
-    # Выполняем команду
-    # Создаем новый экземпляр терминала
+        # Получаем вывод команды
+        output, error = terminal.communicate()
 
-    # Отправляем команду в терминал
-    terminal.stdin.write(text.encode() + b'\n')
-
-    # Получаем вывод команды
-    output, error = terminal.communicate()
-
-    await sendmess("```"+output.decode()+"```", update, context)
+        await context.bot.send_message("```"+output.decode()+"```", chat_id=update.effective_chat.id)
